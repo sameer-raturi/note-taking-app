5e181a5a040aeed55ce739df3fc0e01d7c3cd26c
Sameer_Raturi <sameerraturi2001@gmail.com>
2024-04-02 17:19:34 +0530

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 function App() {
-  return <div>Hi</div>;
+  return (
+    <Container>
+      <Routes>
+        <Route path="/" element={<h1>Home Screen</h1>} />
+        <Route path="/new" element={<h1>New Note</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>id</h1>} />
+          <Route path="edit" element={<h1>editing note</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to={'/'}/>}/>
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
